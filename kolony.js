4411d18f47bc8fc58308ee5421c46c0dec3c4f00
Amit Aswani <amit4got@gmail.com>
2015-04-14 15:48:25 +0100

    Using callbacks to run project install

--- kolony.js
+++ kolony.js
@@ -2,53 +2,61 @@
 // Get CLI params
 var args = process.argv.slice(2, process.argv.length);
 
-var usageString = "Usage: node kolony.js createProject \"Project Path\" \"Project Name\"\n - Project Path is the path (relative or absolute) for the Project\n - Project Name has to be alphanumeric and is the Project Folder Name"
+// Usage string
+var usageString = "Usage: node kolony.js CreateProject \"Project Path\" \"Project Name\"\n - Project Path is the path (relative or absolute) for the Project\n - Project Name has to be alphanumeric and is the Project Folder Name"
 
 // 3 Args ... first CreateProject ... second Project Path ... third Project Name
 if (3 > args.length) {
-	console.log(usageString);
-	process.exit(1);
+    console.log(usageString);
+    process.exit(1);
 }
 
 
 // First arg should be CreateProject
 if ("createproject" !== args[0].toLowerCase()) {
-	console.log(usageString);
-	process.exit(1);
+    console.log(usageString);
+    process.exit(1);
 }
 
 // Second arg should be either Project Path
 // @ToDo check if path exists
 if (! args[1].match(/^[a-zA-Z0-9 -_.\/]+$/)) {
-	console.log(usageString);
-	process.exit(1);
+    console.log(usageString);
+    process.exit(1);
 }
 
 // Third arg should be Project Name 
 if (! args[2].match(/^[a-zA-Z0-9]+$/)) {
-	console.log(usageString);
-	process.exit(1);
+    console.log(usageString);
+    process.exit(1);
 }
 
 // Allow Exec to run git clone and other shell commands
-var exec = require('child_process').exec;
+var child = require('child_process');
 
 // Remove the last / from the project path if it has one
 var project = args[1].replace(/\/$/, "");
-	project = project + "/" + args[2] 
+    project = project + "/" + args[2] 
 
 
 // Ready to create the new project
 console.log('Ready to clone!');
-var gitOutput = exec("git clone https://github.com/KolonyIO/kolony-start-app.git " + project).output;
-if (undefined !== gitOutput) {
-	console.dir(gitOutput);
-}
+child.exec("git clone https://github.com/KolonyIO/kolony-start-app.git " + project, function(error, stdout, stderr) {
+    if (error) throw error;
+    if (stdout) console.dir(stdout);
+    if (stderr) console.dir(stderr);
+
+    // Run install
+    console.log('Installing project');
+    child.exec("cd " + project + " && sh install.sh", function(error, stdout, stderr) {
+        if (error) throw error;
+        if (stdout) console.dir(stdout);
+        if (stderr) console.dir(stderr);
+
+        console.log('Ready to start working in ' + project);
+    });
+
+});
 
 
-// Run install
-console.log('Installing project');
-var runInstall = exec("cd " + project + " && ./install.sh");
-console.log(runInstall);
 
-console.log('Ready to start');
